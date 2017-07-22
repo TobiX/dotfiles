bbdd9f8fb6d5af6888143b4e9103b1921845f2d6
Tobias Gruetzmacher <tobias-git@23.gs>
2017-07-22 17:04:36 +0200

    Relative symlinks are much nicer...

--- setup.py
+++ setup.py
@@ -11,8 +11,9 @@ exit = 0
 for f in glob('dot.*'):
     dst = os.path.expanduser('~/' + f[3:])
     src = os.path.join(os.getcwd(), f)
+    src_rel = os.path.relpath(src, os.path.dirname(dst))
     try:
-        os.symlink(src, dst)
+        os.symlink(src_rel, dst)
     except FileExistsError:
         if not os.path.samefile(src, dst):
             print(src + " exists and does not link do " + dst)
