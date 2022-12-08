6b7405be8411812dd2f0360391d0ac7612d49e61
Tobias Gruetzmacher <tobias-git@23.gs>
2022-12-08 01:04:16 +0100

    Simplify PanDoc table stripping a bit

--- tools/pandoc-table-strip.py
+++ tools/pandoc-table-strip.py
@@ -6,62 +6,50 @@
 from sys import stdin, stdout, stderr
 import json
 
-def extractTable(t, c):
-    if t == 'Table':
-        embeddedElements = []
-        if len(c) < 6:
-            raise NotImplementedError('Sorry, your pandoc is too old')
-        rows = []
-        if c[3]:  # Headers
-            rows.extend(c[3][1])
-        if c[4]:  # Bodies
-            for body in c[4]:
-                rows.extend(body[2])
-                rows.extend(body[3])
-        if c[5]:  # Footer
-            rows.extend(c[5][1])
-        for row in rows:
-            for cell in row[1]:
-                for node in cell[4]:
-                    # Check if we have an embedded table
-                    if 't' in node and node['t'] == 'Table':
-                        embeddedElements.extend(extractTable(node['t'], node['c']))
-                    else:
-                        # Inject empty block to restore formatting inherited by table
-                        block = {'t': 'Plain', 'c': [{'t': 'LineBreak', 'c': []}]}
-                        embeddedElements.append(block)
-                        embeddedElements.append(node)
-        return embeddedElements
-    return None
+def flattenTable(content):
+    embeddedElements = []
+    if len(content) < 6:
+        raise NotImplementedError('Sorry, your pandoc is too old')
+    rows = []
+    if content[3]:  # Headers
+        rows.extend(content[3][1])
+    if content[4]:  # Bodies
+        for body in content[4]:
+            rows.extend(body[2])
+            rows.extend(body[3])
+    if content[5]:  # Footer
+        rows.extend(content[5][1])
+    for row in rows:
+        for cell in row[1]:
+            for node in cell[4]:
+                embeddedElements.append(node)
+    return embeddedElements
 
-def parse(node, func):
+def parse(node):
     if isinstance(node, dict):
         rdict = node.copy()
         for attr in node.keys():
-            rdict[attr] = parse(rdict[attr], func)
+            rdict[attr] = parse(rdict[attr])
         return rdict
-    elif isinstance(node, list):
+    if isinstance(node, list):
         rarray = []
         for item in node:
             if isinstance(item, dict) and 't' in item and 'c' in item:
-                res = func(item['t'], item['c'])
-                if isinstance(res, list):
-                    for rt in res:
-                        rarray.append(parse(rt, func))
+                if item['t'] == 'Table':
+                    rarray.extend(parse(flattenTable(item['c'])))
                 else:
-                    rarray.append(parse(item, func))
+                    rarray.append(parse(item))
             else:
-                rarray.append(parse(item, func))
+                rarray.append(parse(item))
         return rarray
-    else:
-        return node
+    return node
 
 
 
 def main():
     jstr = stdin.read()
     tree = json.loads(jstr)
-    tree = parse(tree, extractTable)
+    tree = parse(tree)
     stdout.write(json.dumps(tree))
 
 if __name__ == '__main__':
